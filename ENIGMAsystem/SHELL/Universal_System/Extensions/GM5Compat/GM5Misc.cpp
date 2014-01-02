4776d3959a00e0b7709c40c6d4989f10dcf1af7b
Seth N. Hetu <seth.hetu@gmail.com>
2014-01-02 00:38:41 -0500

    Added enough wrapper functions to get draw_rectangle() working flawlessly in GM5 compat mode. The use of magic global variables has still not been addressed.

--- ENIGMAsystem/SHELL/Universal_System/Extensions/GM5Compat/GM5Misc.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/GM5Compat/GM5Misc.cpp
@@ -4,6 +4,30 @@
 
 #include "GM5Misc.h"
 
+#include <cmath>
+#include <algorithm>
+
+#include "Graphics_Systems/General/GSstdraw.h"
+#include "Graphics_Systems/General/GScolors.h"
+
+namespace {
+
+//GM5 draw state.
+int pen_color = enigma_user::c_black;
+gs_scalar pen_size = 1;
+int brush_color = enigma_user::c_black;
+int brush_style = enigma_user::bs_solid; 
+
+
+template <typename T>
+void swap(T& val1, T& val2){
+  T temp = val1;
+  val1 = val2;
+  val2 = temp;
+}
+}
+
+
 namespace enigma_user
 {
 
@@ -13,6 +37,60 @@ void set_graphics_mode(bool exclusive, int horizRes, int colorDepth, int freq, b
 
 void screen_gamma(double r, double g, double b)
 {
+}
+
+void draw_rectangle(gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2)
+{
+  //lwid/hwid are the width and half-width, respectively. shft is used to nudge odd-width penned shapes to the right.
+  int lwid = std::max(1,(int)round(pen_size));
+  int hwid = lwid/2;
+  int shft = (lwid%2==0)?0:1;
+
+  //Ensure (x1,y1) are the smallest components, and (x2,y2) are the largest.
+  if (x2<x1) { swap(x1,x2); }
+  if (y2<y1) { swap(y1,y2); }
+
+  //Nudge the shape into position.
+  x1 = int(x1) + shft;
+  x2 = int(x2) + shft - 1 ;
+  y1 = int(y1);
+  y2 = int(y2) - 1 ;
+
+  //Fill the shape, if we have the correct brush style.
+  //Note: At the moment, we treat all unsupported brush styles as "solid"; only "hollow" avoids drawing.
+  if (brush_style != bs_hollow) {
+    draw_set_color(brush_color);
+    draw_rectangle(x1,y1,x2,y2,false);
+  }
+
+  //Draw the line (this always happens; a pen_size of 0 still draws a 1px line).
+  //Note: Subtracting shift from one x-component worries me; will have to confirm on Windows that this
+  //      is not a bug in the OpenGL code.
+  draw_set_color(pen_color);
+  draw_line_width(x1,           y1-hwid, x1,      y2+hwid, lwid);
+  draw_line_width(x1-hwid-shft, y2,      x2+hwid, y2,      lwid);
+  draw_line_width(x2,           y2+hwid, x2,      y1-hwid, lwid);
+  draw_line_width(x2+hwid,      y1,      x1-hwid, y1,      lwid);
+}
+
+void draw_set_pen_color(int clr)
+{
+  pen_color = clr;
+}
+
+void draw_set_pen_size(gs_scalar sz)
+{
+  pen_size = sz;
+}
+
+void draw_set_brush_color(int clr)
+{
+  brush_color = clr;
+}
+
+void draw_set_brush_style(int sty)
+{
+  brush_style = sty;
 }
 
 void draw_text_sprite(int x, int y, string str, int sep, int w, int sprite, int firstChar, int scale)
--- ENIGMAsystem/SHELL/Universal_System/Extensions/GM5Compat/GM5Misc.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/GM5Compat/GM5Misc.h
@@ -6,14 +6,40 @@
 #define ENIGMA_GM5COMPAT_MISC__H
 
 #include "Universal_System/var4.h"
+#include "Universal_System/scalar.h"
 
 
 namespace enigma_user
 {
 
+//Brush styles.
+enum {
+  bs_solid  = 0,
+  bs_hollow,
+
+  //Currently not supported.
+  bs_bdiagonal,
+  bs_fdiagonal,
+  bs_cross,
+  bs_diagcross,
+  bs_horizontal,
+  bs_vertical,
+};
+
+
 //General graphics
 void set_graphics_mode(bool exclusive, int horizRes, int colorDepth, int freq, bool fullscreen, int winScale, int fullScale);
 void screen_gamma(double r, double g, double b);
+
+//Drawing - Wrappers.
+void draw_rectangle(gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2);
+
+//Drawing - Related
+void draw_set_pen_color(int clr);
+void draw_set_pen_size(gs_scalar sz);
+void draw_set_brush_color(int clr);
+void draw_set_brush_style(int sty);
+
 
 //Drawing - TextSprite
 void draw_text_sprite(int x, int y, string str, int sep, int w, int sprite, int firstChar, int scale);
